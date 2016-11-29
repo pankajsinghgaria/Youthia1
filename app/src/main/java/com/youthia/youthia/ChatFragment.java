aa4608730d51bffbff1b4aab3bad8b8d71ed3ed1
ANMOL KOHLI <anmolrocker32@gmail.com>
2016-11-29 21:44:28 +0530

    Test

--- app/src/main/java/com/youthia/youthia/ChatFragment.java
+++ app/src/main/java/com/youthia/youthia/ChatFragment.java
@@ -8,15 +8,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by garia on 21-11-2016.
- */
+
 
 public class ChatFragment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.chats_fragment_layout, container, false);
+
+        //Checkout testing
+        View view;
+        view= inflater.inflate(R.layout.chats_fragment_layout,container,false);
+        return view;
     }
 }
